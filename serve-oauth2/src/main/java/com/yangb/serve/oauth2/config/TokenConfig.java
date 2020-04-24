8541871cb2abeda007145b390cddee1f7ae8b524
yangb <523084685@qq.com>
2020-04-24 17:41:36 +0800

    :sparkles: 将Token存储从 jdbc 更改为 JWT

--- serve-oauth2/src/main/java/com/yangb/serve/oauth2/config/TokenConfig.java
+++ serve-oauth2/src/main/java/com/yangb/serve/oauth2/config/TokenConfig.java
@@ -10,11 +10,14 @@ import org.springframework.security.oauth2.provider.code.AuthorizationCodeServic
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 /**
  * Created by yangb on 2020/4/24
@@ -23,14 +26,26 @@ import javax.sql.DataSource;
 @Configuration
 public class TokenConfig {
 
+    private static final String SIGN_KEY = "yangb";
+
     @Resource
     private TokenStore tokenStore;
     @Resource
     private BCryptPasswordEncoder passwordEncoder;
+    @Resource
+    JwtAccessTokenConverter jwtAccessTokenConverter;
 
     @Bean
-    public TokenStore tokenStore(DataSource dataSource) {
-        return new JdbcTokenStore(dataSource);
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    // 令牌转换器
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGN_KEY); // 对称密匙
+        return converter;
     }
 
     // 密码编码器
@@ -53,6 +68,10 @@ public class TokenConfig {
         services.setClientDetailsService(clientDetailsService); // 客户端详情服务
         services.setSupportRefreshToken(true); // 支持令牌刷新
         services.setTokenStore(tokenStore); // 令牌存储策略
+        // 令牌增强
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtAccessTokenConverter));
+        services.setTokenEnhancer(tokenEnhancerChain);
         services.setAccessTokenValiditySeconds(7200); // 令牌有效期2小时
         services.setRefreshTokenValiditySeconds(259200); // 刷新令牌默认有效期3天
         return services;
@@ -65,4 +84,5 @@ public class TokenConfig {
         ((JdbcClientDetailsService) clientDetailsService).setPasswordEncoder(passwordEncoder);
         return clientDetailsService;
     }
+
 }
