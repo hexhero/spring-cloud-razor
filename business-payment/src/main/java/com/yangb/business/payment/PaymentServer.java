4f1873d7a827044ea37c43c3acf2ab0fa37c727c
yangb <523084685@qq.com>
2020-04-26 13:31:01 +0800

    :wrench: 开启服务发现配置

--- business-payment/src/main/java/com/yangb/business/payment/PaymentServer.java
+++ business-payment/src/main/java/com/yangb/business/payment/PaymentServer.java
@@ -3,6 +3,7 @@ package com.yangb.business.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -12,6 +13,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("com.yangb.business.payment.dao")
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 public class PaymentServer {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServer.class, args);
